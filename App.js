a1cb9284eccd93da3834409bad7d26deb3550b2b
Sara <saraesalazar@icloud.com>
2020-11-01 13:43:43 -0500

    added an src folder with the following folders: components, navigations, scences, styles, utils

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+      <View>
+        <TextInput
+          label="Email"
+          style={{
+            height: 40,
+            width: 250,
+            borderColor: "gray",
+            borderWidth: 1,
+            margin: 5,
+          }}
+          placeholder="Email"
+        ></TextInput>
+        <TextInput
+          style={{
+            height: 40,
+            width: 250,
+            borderColor: "gray",
+            borderWidth: 1,
+            margin: 5,
+          }}
+          placeholder="Password"
+        ></TextInput>
+      </View>
+      <View>
+        <Button
+          onPress={() => alert("You pressed me!")}
+          title="Submit"
+          color="purple"
+          style={styles.button}
+        ></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +49,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "white",
+  },
+  text: {
+    color: "white",
+  },
+  button: {
+    margin: "20px",
+    height: "10px",
+    color: "gray",
   },
 });
